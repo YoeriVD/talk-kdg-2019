5ae68c0345dcf6618fb28178664f2e4996993146
YoeriVD <yoeri.vandamme@gmail.com>
2018-03-14 21:40:57 +0100

    display templates

--- demo/mvc-demo/Controllers/HomeController.cs
+++ demo/mvc-demo/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using mvc_demo.Models;
 
@@ -13,36 +10,84 @@ namespace mvc_demo.Controllers
         {
             var model = new HomeIndexViewModel
             {
-                TextCards = new List<TextCard>()
+                TextCards = new List<TextCard>
                 {
-                    new TextCard()
+                    new TextCard
                     {
                         Title = "This is the first text",
                         Preview =
                             "Met video's kunt u uw boodschap krachtig overbrengen. Wanneer u op Onlinevideo klikt, kunt u de ingesloten code plakken voor de video die u wilt toevoegen. U kunt ook een trefwoord typen om online te zoeken naar de video die het meest geschikt is voor het document. Als u het document een professioneler uiterlijk wilt geven, kunt u in Word kiezen uit koptekst-, voettekst-, voorblad- en tekstvakontwerpen die elkaar aanvullen. U kunt bijvoorbeeld een overeenkomend voorblad, koptekst en sidebar toevoegen. Klik op Invoegen en kies vervolgens de elementen uit de verschillende galerieën. Met thema's en stijlen kunt u ook uw document op orde houden. Wanneer u op Ontwerpen klikt en een nieuw thema kiest, worden de afbeeldingen, grafieken en SmartArt-afbeeldingen aan uw nieuwe thema aangepast. Wanneer u stijlen toepast, worden kopteksten aangepast aan het nieuwe thema. ",
-                        Sentiments = new[] {"neutral", "motivational"}
+                        Sentiments = new List<Sentiment>
+                        {
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Neutral,
+                                EmotionName = "neutral"
+                            },
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Positive,
+                                EmotionName = "motivational"
+                            }
+                        }
                     },
-                    new TextCard()
+                    new TextCard
                     {
                         Title = "This is the first text",
                         Preview =
                             "Met video's kunt u uw boodschap krachtig overbrengen. Wanneer u op Onlinevideo klikt, kunt u de ingesloten code plakken voor de video die u wilt toevoegen. U kunt ook een trefwoord typen om online te zoeken naar de video die het meest geschikt is voor het document. Als u het document een professioneler uiterlijk wilt geven, kunt u in Word kiezen uit koptekst-, voettekst-, voorblad- en tekstvakontwerpen die elkaar aanvullen. U kunt bijvoorbeeld een overeenkomend voorblad, koptekst en sidebar toevoegen. Klik op Invoegen en kies vervolgens de elementen uit de verschillende galerieën. Met thema's en stijlen kunt u ook uw document op orde houden. Wanneer u op Ontwerpen klikt en een nieuw thema kiest, worden de afbeeldingen, grafieken en SmartArt-afbeeldingen aan uw nieuwe thema aangepast. Wanneer u stijlen toepast, worden kopteksten aangepast aan het nieuwe thema. ",
-                        Sentiments = new[] {"neutral", "motivational"}
+                        Sentiments = new List<Sentiment>
+                        {
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Neutral,
+                                EmotionName = "neutral"
+                            },
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Positive,
+                                EmotionName = "motivational"
+                            }
+                        }
                     },
-                    new TextCard()
+                    new TextCard
                     {
                         Title = "This is the first text",
                         Preview =
                             "Met video's kunt u uw boodschap krachtig overbrengen. Wanneer u op Onlinevideo klikt, kunt u de ingesloten code plakken voor de video die u wilt toevoegen. U kunt ook een trefwoord typen om online te zoeken naar de video die het meest geschikt is voor het document. Als u het document een professioneler uiterlijk wilt geven, kunt u in Word kiezen uit koptekst-, voettekst-, voorblad- en tekstvakontwerpen die elkaar aanvullen. U kunt bijvoorbeeld een overeenkomend voorblad, koptekst en sidebar toevoegen. Klik op Invoegen en kies vervolgens de elementen uit de verschillende galerieën. Met thema's en stijlen kunt u ook uw document op orde houden. Wanneer u op Ontwerpen klikt en een nieuw thema kiest, worden de afbeeldingen, grafieken en SmartArt-afbeeldingen aan uw nieuwe thema aangepast. Wanneer u stijlen toepast, worden kopteksten aangepast aan het nieuwe thema. ",
-                        Sentiments = new[] {"neutral", "motivational"}
+                        Sentiments = new List<Sentiment>
+                        {
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Neutral,
+                                EmotionName = "neutral"
+                            },
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Positive,
+                                EmotionName = "motivational"
+                            }
+                        }
                     },
-                    new TextCard()
+                    new TextCard
                     {
                         Title = "This is the first text",
                         Preview =
                             "Met video's kunt u uw boodschap krachtig overbrengen. Wanneer u op Onlinevideo klikt, kunt u de ingesloten code plakken voor de video die u wilt toevoegen. U kunt ook een trefwoord typen om online te zoeken naar de video die het meest geschikt is voor het document. Als u het document een professioneler uiterlijk wilt geven, kunt u in Word kiezen uit koptekst-, voettekst-, voorblad- en tekstvakontwerpen die elkaar aanvullen. U kunt bijvoorbeeld een overeenkomend voorblad, koptekst en sidebar toevoegen. Klik op Invoegen en kies vervolgens de elementen uit de verschillende galerieën. Met thema's en stijlen kunt u ook uw document op orde houden. Wanneer u op Ontwerpen klikt en een nieuw thema kiest, worden de afbeeldingen, grafieken en SmartArt-afbeeldingen aan uw nieuwe thema aangepast. Wanneer u stijlen toepast, worden kopteksten aangepast aan het nieuwe thema. ",
-                        Sentiments = new[] {"neutral", "motivational"}
-                    },
+                        Sentiments = new List<Sentiment>
+                        {
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Neutral,
+                                EmotionName = "neutral"
+                            },
+                            new Sentiment
+                            {
+                                EmotionKind = EmotionKind.Positive,
+                                EmotionName = "motivational"
+                            }
+                        }
+                    }
                 }
             };
             return View(model);
--- demo/mvc-demo/Models/HomeIndexViewModel.cs
+++ demo/mvc-demo/Models/HomeIndexViewModel.cs
@@ -11,6 +11,19 @@ namespace mvc_demo.Models
     {
         public string Title { get; set; }
         public string Preview { get; set; }
-        public string[] Sentiments { get; set; }
+        public List<Sentiment> Sentiments { get; set; }
+    }
+
+    public class Sentiment
+    {
+        public string EmotionName { get; set; }
+        public EmotionKind EmotionKind { get; set; }
+    }
+
+    public enum EmotionKind
+    {
+        Positive = 1,
+        Neutral = 0,
+        Negative = -1
     }
 }
