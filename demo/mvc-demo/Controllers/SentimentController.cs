894898a96e8daa167a68d87cf6e4756e14039db9
YoeriVD <yoeri.vandamme@gmail.com>
2018-03-14 21:29:04 +0100

    cards toegevoegd

---
+++ demo/mvc-demo/Controllers/SentimentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_demo.Models
+{
+    public partial class SentimentController : Controller
+    {
+        // GET: Sentiment
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ demo/mvc-demo/Models/HomeIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace mvc_demo.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<TextCard> TextCards { get; set; }
+    }
+
+    public class TextCard
+    {
+        public string Title { get; set; }
+        public string Preview { get; set; }
+        public string[] Sentiments { get; set; }
+    }
+}
